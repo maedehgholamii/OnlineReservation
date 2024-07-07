1d062e310d0131897ad48218ac0e5aacf7a7c3d7
Maedeh <Maedeh.Gholami24@gmail.com>
2024-07-07 12:53:16 +0330

    create controller - exceptions

---
+++ controller/exceptions/exceptions.py
@@ -0,0 +1,19 @@
+class CustomerNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Customer Not Found !!!")
+
+class Beauty_JobNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Beauty Job Not Found !!!")
+
+class EmployeeNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Employee Not Found !!!")
+
+class ReserveotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Reserve Not Found !!!")
+
+class TimingNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Timing Not Found !!!")
