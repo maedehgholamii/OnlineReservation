f955722b8373fbf08f001fd2aee76d46bc115c71
Maedeh <Maedeh.Gholami24@gmail.com>
2024-07-22 00:41:41 +0330

    updated

--- view/component/login.py
+++ view/component/login.py
@@ -6,27 +6,25 @@ from model.entity.customer import Customer
 from model.entity.employee import Employee
 from model.entity.reserve import Reserve
 from model.entity.timing import Timing
-from tkinter import *
-import re
-from tkinter import *
+import tkinter as tk
 import tkinter.messagebox as msg
-import tkinter.ttk as tk
+from tkinter import ttk
 
 class OnlineReservationView:
-    # def beauty_job_table_click(self,row):
-    #     print (row)
-    #
-    # def customer_table_click(self, row):
-    #     print(row)
-    #
-    # def employee_table_click(self, row):
-    #     print(row)
-    #
-    # def reserve_table_click(self, row):
-    #     print(row)
-    #
-    # def timing_table_click(self, row):
-    #     print(row)
+    def beauty_job_table_click(self, row):
+        print(row)
+
+    def customer_table_click(self, row):
+        print(row)
+
+    def employee_table_click(self, row):
+        print(row)
+
+    def reserve_table_click(self, row):
+        print(row)
+
+    def timing_table_click(self, row):
+        print(row)
 
     def __init__(self):
         self.beauty_job_da = DataAccess(BeautyJob)
@@ -34,52 +32,50 @@ class OnlineReservationView:
         self.employee_da = DataAccess(Employee)
         self.timing_da = DataAccess(Timing)
         self.reserve_da = DataAccess(Reserve)
-        self.win = Tk()
+
+        self.win = tk.Tk()
         self.win.title("Login")
         self.win.geometry("500x500")
-        self.username = TextWithLabel(self.win,"username",x = 20 , y = 20)
-        self.password = TextWithLabel(self.win,"password",x = 20 , y = 50)
-        Button(self.win,text='vorod',command=self.check, bg = "lightblue").place(x=60,y=80)
-        # self.beauty_job_table = Table(self.win,
-        #                      ["Id", "Title", "Image", "Description"],
-        #                      [60,80,80,80,50], 20, 20 ,
-        #                      self.beauty_job_table_click)
-        #
-        # self.beauty_job_table.refresh_table(self.beauty_job_da.find_all())
-        # self.customer_table = Table(self.win,
-        #                      ["Id","Name","Family","Mobile", "Instagram_Id", "Telegram_id"],
-        #                      [60,80,80,80,50,60,60],300,20 ,
-        #                      self.customer_table_click)
-        #
-        #
-        #
-        # user_list = [("maedeh", "maedeh"), ("navid", "navid")]
-        #
-        # def login():
-        #     if (username.get(), password.get()) in user_list:
-        #         msg.showinfo("Login", "Welcome")
-        #
-        #     else:
-        #         msg.showerror("Access Denied", "Invalid Username or Password")
 
+        self.username = TextWithLabel(self.win, "username", x=20, y=20)
+        self.password = TextWithLabel(self.win, "password", x=20, y=50)
+        tk.Button(self.win, text='vorod', command=self.check, bg="lightblue").place(x=60, y=80)
 
-        #
-        # # Id
-        # Label(win, text="Username").place(x=20, y=20)
-        # username = StringVar()
-        # username_txt = Entry(win, textvariable=username)
-        # username_txt.place(x=100, y=20)
-        #
-        # # Name
-        # Label(win, text="Password").place(x=20, y=70)
-        # password = StringVar()
-        # password_txt = Entry(win, textvariable=password)
-        # password_txt.place(x=100, y=70)
-        #
-        # Button(win, text="Login", width=10, command=login).place(x=60, y=170)
+        self.beauty_job_table = Table(self.win,
+                                      ["Id", "Title", "Image", "Description"],
+                                      [60, 80, 80, 80, 50], 20, 120,  # Adjusted y position
+                                      self.beauty_job_table_click)
+        self.beauty_job_table.refresh_table(self.beauty_job_da.find_all())
 
-        self.win.mainloop()
+        self.customer_table = Table(self.win,
+                                    ["Id", "Name", "Family", "Mobile", "Instagram_Id", "Telegram_id"],
+                                    [60, 80, 80, 80, 50, 60, 60], 300, 120,  # Adjusted y position
+                                    self.customer_table_click)
+        self.customer_table.refresh_table(self.customer_da.find_all())
+
+        self.employee_table = Table(self.win, ["Id", "Name", "Family", "Mobile", "Instagram Id", "Telegram_id"],
+                                    [60, 80, 80, 80, 50, 60, 60], 20, 250,  # Adjusted y position
+                                    self.employee_table_click)
+        self.employee_table.refresh_table(self.employee_da.find_all())
+
+        self.timing_table = Table(self.win, ["Shift Date", "Start Time", "End Time"],
+                                  [60, 80, 80], 300, 250,  # Adjusted y position
+                                  self.timing_table_click)
+        self.timing_table.refresh_table(self.timing_da.find_all())
+
+        self.reserve_table = Table(self.win, ["Shift Date", "Start Time", "End Time"],
+                                   [60, 80, 80], 20, 380,  # Adjusted y position
+                                   self.reserve_table_click)
+        self.reserve_table.refresh_table(self.reserve_da.find_all())
+
+        self.total = TextWithLabel(self.win, "total", 400, 450)
 
     def check(self):
-        print(self.username.text,self.password.text)
-ui = OnlineReservationView()
+        username = self.username.get_text()
+        password = self.password.get_text()
+
+        print(f"Username: {username}, Password: {password}")
+
+
+app = OnlineReservationView()
+app.win.mainloop()
