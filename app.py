cd649ea4a4f1afa3bdc708777296b5ed087622e3
Maedeh <Maedeh.Gholami24@gmail.com>
2024-08-03 22:09:04 +0330

    updated

--- app.py
+++ app.py
@@ -1,4 +1,45 @@
-
+from view.customer_view import CustomerView
+from view.employee_view import EmployeeView
 from view.reserve_view import ReserveView
+# print("App started")
+# ui = PersonView()
+# ui.show()
+# print("App ended")
 
-ui = ReserveView()
+from tkinter import *
+import tkinter.messagebox as msg
+import tkinter.ttk as ttk
+from view.component.label_text import TextWithLabel
+
+
+class FrontView:
+
+    def show_view_customer(self):
+        msg.showinfo("question", "برای ایجاد حساب  ابتدا مشخصات خود را وارد کنید ")
+        ui = CustomerView()
+        ui.show()
+
+    def show_view_reserve(self):
+        ui = ReserveView()
+        ui.show()
+
+    def show(self):
+        self.win = Tk()
+        self.win.title("View")
+        self.win.geometry("200x200")
+
+        Button(self.win, text="ایجاد حساب کاربری", command=self.show_view_customer).place(x=20, y=20)
+
+        Button(self.win, text="reserve", command=self.show_view_reserve).place(x=20, y=60)
+
+        # self.table = ttk.Treeview(self.win, columns=(1,2), show="headings")
+
+        # self.table.place(x=320, y=20)
+
+        # self.reset_form()
+
+        self.win.mainloop()
+
+
+ui = CustomerView()
+ui.show()
