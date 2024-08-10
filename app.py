d973ba244ebf8d5e607a9b0a980bd3d93a834936
Maedeh <Maedeh.Gholami24@gmail.com>
2024-08-10 18:28:19 +0330

    updated

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
+from view.beauty_job_view import BeautyJobView
 from view.reserve_view import ReserveView
 from view.customer_view import CustomerView
-# print("App started")
-# ui = PersonView()
-# ui.show()
-# print("App ended")
-
 from tkinter import *
 import tkinter.messagebox as msg
 import tkinter.ttk as ttk
@@ -14,31 +10,36 @@ from view.component.label_text import TextWithLabel
 class FrontView:
 
     def show_view_customer(self):
-        msg.showinfo("question", "برای ایجاد حساب بانکی ابتدا مشخصات خود را وارد کنید ")
+        msg.showinfo("question", "برای ورود به حساب کاربری ابتدا مشخصات خود را وارد کنید ")
         ui = CustomerView()
-
+        ui.show()
 
     def show_view_reserve(self):
         ui = ReserveView()
+        ui.show()
 
+    def show_view_beauty_job(self):
+        ui = BeautyJobView()
+        ui.show()
 
     def show(self):
         self.win = Tk()
         self.win.title("View")
         self.win.geometry("200x200")
 
-        Button(self.win, text="ایجاد حساب کاربری", command=self.show_view_customer).place(x=20, y=20)
+        frame = Frame(self.win, bd=2, relief="sunken")
+        frame.place(x=10, y=10, width=300, height=180)
 
-        Button(self.win, text="reserve", command=self.show_view_reserve).place(x=20, y=60)
+        Button(frame,text="ورود حساب کاربری", command=self.show_view_customer).place(x=20, y=20)
 
-        self.table = ttk.Treeview(self.win, columns=(1,2), show="headings")
+        Button(frame, text="رزرو", command=self.show_view_reserve).place(x=20, y=60)
+
+        self.table = ttk.Treeview(self.win, columns=(1, 2), show="headings")
 
         self.table.place(x=320, y=20)
-
-        self.reset_form()
 
         self.win.mainloop()
 
 
-ui = CustomerView()
+ui = FrontView()
 ui.show()
---
+++ readme.md
@@ -0,0 +1,14 @@
+[!NOTE] HERE IS INSTALLATION PROMPT FOR THIS CODE :"
+
+- pip install mysql-connector-python
+- pip install sqlalchemy
+- pip install sqlalchemy_utils
+- pip install cryptography
+- pip install pymysql
+For Install You Need to -Turn On Your Vpn
+
+Press Win + R
+Type CMD and Press ENTER
+Cope and Paste Promp in it
+Press ENTER
+[!IMPORTANT] First You Need to Install Prompt to Run Code
